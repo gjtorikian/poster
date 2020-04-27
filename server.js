1aee0701e6984d49cf7ffdabdbd185a3e80d057e
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2020-04-27 16:19:09 +0100

    All CORS

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ const port = 5591;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
